041f358ae107daadf930a786683d1890d89c170d
Jesse-Bett <jessekip21@gmail.com>
2022-05-18 19:50:57 +0300

    Addded page 1 - "Intro"
    finished the layout of the landing page - to be refactored ✌︎

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <header className="App-header"></header>
+
+      <section id ="IntroPage">
+      <div id = 'leftHalf'></div>
+      <div id = 'RightHalf'>
+
+        <h1 class ='Headings' id = 'intro' > Hello there, Welcome to my Portfolio ! </h1>
+        
+        
+        <p id = "paragraph">I'm a software developer whose primary goal is to apply technical expertise all throughout the full software life cycle to ensure  the  production and delivery  of world-class products and services that meet client specifications. Along with a competent software developing team, and with strong personal knowledge,  skills,  and experience  in  software engineering, I am positive that this goal can be achieved. My experience as a junior software developer enhanced my abilities in designing,implementing, testing, and upgrading software. One of my objectives is to keep
+updated with the latest tech trends. I am confident that
+ if given the opportunity, I can be a useful talent to the company.</p>
+
+
+        <p id = "Regards">Regards, <br/>Jesse Bett</p>
+      </div>
+      </section>
+
+
+      
     </div>
   );
 }
