eeea7802f4493025a7b1209c9470e5858a9cc23b
Jesse-Bett <jessekip21@gmail.com>
2022-05-25 15:11:53 +0300

    Added flex possibility

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import './App.css';
-//const htmlfile = require('./index.html');
+
 
 function App() {
   return (
     <div className="App">
-     <header className="App-header"></header>
+     <header className ="App-header"></header>
      
-       
-    </div>
+      <section>
+        <div  id ="IntroPage" >
+        <div id = 'leftHalf'></div>
+        <div id = 'RightHalf'>
+  
+          <h1 className='Headings' id = 'intro' > Hello there, Welcome to my Portfolio ! </h1>
+          
+          
+          <p id = "paragraph">I'm a software developer whose primary goal is to apply technical expertise all throughout the full software life cycle to ensure  the  production and delivery  of world products and services that meet client specifications. Along with a competent software developing team, and with strong personal knowledge,  skills,  and experience  in  software engineering, I am positive that this goal can be achieved. My experience as a junior software developer enhanced my abilities in designing,implementing, testing, and upgrading software. One of my objectives is to keep
+  updated with the latest tech trends. I am confident that
+   if given the opportunity, I can be a useful talent to the company.</p>
+  
+  
+          <p id = "Regards">Regards, <br/>Jesse Bett</p>
+        </div>
+        </div>
+        </section>
+
+        <section id="Projects">
+          
+        </section>
+  
+    </div> 
+    
   );
 }
 
 export default App;
-    // <iframe src={htmlfile}></iframe>
+  
