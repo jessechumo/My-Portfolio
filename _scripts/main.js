00e3dd6ac73b93b1f959e228a4ad398e739ffe4f
jesse-bett <jessekip21@gmail.com>
2025-12-03 12:17:36 -0600

    add my info

--- _scripts/main.js
+++ _scripts/main.js
@@ -40,26 +40,7 @@ $(function() {
     $('html, body').animate({ scrollTop: 0 }, 500);
   });
 
-  const hand = document.querySelector('.emoji.wave-hand');
-
-  function waveOnLoad() {
-    hand.classList.add('wave');
-    setTimeout(function() {
-      hand.classList.remove('wave');
-    }, 2000);
-  }
-
-  setTimeout(function() {
-    waveOnLoad();
-  }, 1000);
-
-  hand.addEventListener('mouseover', function() {
-    hand.classList.add('wave');
-  });
-
-  hand.addEventListener('mouseout', function() {
-    hand.classList.remove('wave');
-  });
+  // Wave emoji animation removed - now using Unicode emojis
 
   window.sr = ScrollReveal({
     reset: false,
